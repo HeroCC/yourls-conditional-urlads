866607f14813bde257d9849afad0f0d9ccf6762b
8Mi_Yile <admin@8mi.tech>
2023-11-27 14:33:22 +0800

    add: settings.php example

--- settings.php
+++ settings.php
@@ -10,28 +10,40 @@ if( !defined( 'YOURLS_ABSPATH' ) ) die();
             <tr valign="center" algin="char">
                 <th>Website</th>
                 <th>&nbsp;&nbsp;ID</th>
+                <th>String</th>
+                <th>Example URL</th>
             </tr>
             <tr valign="center">
                 <th scope="row">Adfly</th>
                 <td><input type="text" name="adfly_id" value="<?php echo yourls_get_option('conditional_urlads_adfly_id'); ?>" /></td>
+                <th>a/</th>
+                <th><a href="https://8mi.ink/a/ref-adfly">https://8mi.ink/a/ref-adfly</a></th>
             </tr>
             <tr valign="center">
                 <th scope="row">Adfoc.us</th>
                 <td><input type="text" name="adfoc_id" value="<?php echo yourls_get_option('conditional_urlads_adfoc_id'); ?>" /></td>
+                <th>f/</th>
+                <th><a href="https://8mi.ink/f/ref-adfoc">https://8mi.ink/f/ref-adfoc</a></th>
             </tr>
             <tr valign="center">
                 <th scope="row">OUO.io</th>
                 <td><input type="text" name="ouoio_id" value="<?php echo yourls_get_option('conditional_urlads_ouoio_id'); ?>" /></td>
+                <th>o/</th>
+                <th><a href="https://8mi.ink/o/ref-ouoio">https://8mi.ink/o/ref-ouoio</a></th>
             </tr>
             <tr valign="center">
                 <th scope="row"><a href="https://publisher.linkvertise.com/dashboard#dynamic">Linkvertise</a></th>
                 <td><input type="text" name="linkvertise_id" value="<?php echo yourls_get_option('conditional_urlads_linkvertise_id'); ?>" /></td>
+                <th>l/</th>
+                <th><a href="https://8mi.ink/l/ref-linkvertise">https://8mi.ink/l/ref-linkvertise</a></th>
             </tr>
             <tr valign="center">
                 <th scope="row">Random AdUrl</th>
                 <td>
                     <input type="checkbox" name="random_adurl_bool" value="true" <?php echo yourls_get_option('conditional_urlads_random_adurl_bool') ? 'checked' : ''; ?> />
                 </td>
+                <th>r/</th>
+                <th><a href="https://8mi.ink/r/2345">https://8mi.ink/r/2345</a></th>
             </tr>
         </table>
         <br />
