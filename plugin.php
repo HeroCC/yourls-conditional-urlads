206fbb29223bcc84509e77f5c3cd57d11aa524df
8Mi_Yile <admin@8mi.tech>
2023-11-28 16:56:33 +0800

    add: Better WebUI and Save button

--- plugin.php
+++ plugin.php
@@ -1,113 +1,134 @@
 <?php
 /*
-Plugin Name: Conditional URL Advertisements
-Plugin URI: https://github.com/HeroCC/yourls-conditional-urlads
+Plugin Name: Conditional URLAds
+Plugin URI: https://github.com/8Mi-Tech/yourls-conditional-urlads
 Description: Conditionally send shortlinks through various link monetizing services
 Version: 1.2.1
-Author: HeroCC
-Author URI: https://herocc.com/
+Author: 8Mi-Tech
+Author URI: https://8mi.ink
 */
 
-if( !defined( 'YOURLS_ABSPATH' ) ) die();
+if ( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-yourls_add_action( 'plugins_loaded', 'conditional_urlads_addpage' );
-function conditional_urlads_addpage() {
-  yourls_register_plugin_page( 'conditional_urlads_settings', 'Conditional URLAds', 'conditional_urlads_loadpage' );
-}
-function conditional_urlads_loadpage(){
-  if (isset( $_POST['nonce'])){
-    yourls_verify_nonce( 'conditional_urlads_settings' );
-    #if( isset( $_POST['adfly_id'], $_POST['adfoc_id'], $_POST['ouoio_id'], $_POST['linkvertise_id'], ) ) 
-    if (isset($_POST['adfly_id'])){
-      yourls_update_option( 'conditional_urlads_adfly_id', $_POST['adfly_id'] );
-    }
-    if (isset($_POST['adfoc_id'])){
-      yourls_update_option( 'conditional_urlads_adfoc_id', $_POST['adfoc_id'] );
-    }
-    if (isset($_POST['ouoio_id'])){
-      yourls_update_option( 'conditional_urlads_ouoio_id', $_POST['ouoio_id'] );
-    }
-    if (isset($_POST['linkvertise_id'])){
-      yourls_update_option( 'conditional_urlads_linkvertise_id', $_POST['linkvertise_id'] );
-    }
-    
-    yourls_update_option( 'conditional_urlads_random_adurl_bool', isset($_POST['random_adurl_bool']));
-  }    
-  #$nonce = yourls_create_nonce( 'conditional_urlads_settings' );
-  include 'settings.php';
-}
-define( 'ADFLY_ID', yourls_get_option( 'conditional_urlads_adfly_id' ) ); // Replace this with your Adfly ID
-define( 'ADFOCUS_ID', yourls_get_option( 'conditional_urlads_adfoc_id' ) ); // Replace this with your Adfoc.us ID
-define( 'OUO_ID', yourls_get_option( 'conditional_urlads_ouoio_id' ) ); // You get the drill
-define( 'LINKVERTISE_ID', yourls_get_option( 'conditional_urlads_linkvertise_id' ) ); // You get the drill
-define( 'RANDOM_ADURL_BOOL', yourls_get_option( 'conditional_urlads_random_adurl_bool' ) );
-define( 'ADFLY_DOMAIN', 'https://adf.ly' ); // If you have a custom Adfly domain, replace this with it
-define( 'ADFOCUS_DOMAIN', 'https://adfoc.us' ); // Same for this
-define( 'OUO_DOMAIN', 'https://ouo.io' ); 
-
-// Trigger when all plugins are loaded
 yourls_add_action( 'plugins_loaded', 'conditional_urlads_load_textdomain' );
 
-// load custom text domain
 function conditional_urlads_load_textdomain() {
-    echo yourls_load_custom_textdomain( 'conditional_urlads', dirname( __FILE__ ) . '/languages' );
+    yourls_load_custom_textdomain( 'conditional_urlads', dirname( __FILE__ ) . '/languages' );
 }
 
-yourls_add_action( 'loader_failed', 'check_for_redirect' ); // On url fail, check here
+yourls_add_action( 'plugins_loaded', 'conditional_urlads_addpage' );
+
+function conditional_urlads_addpage() {
+    if ( $_SERVER[ 'REQUEST_METHOD' ] === 'POST' && $_SERVER["QUERY_STRING"] === 'page=conditional-urlads' ) {
+        conditional_urlads_process_request();
+    } else {
+        yourls_register_plugin_page( 'conditional-urlads', 'Conditional URLAds', 'conditional_urlads_loadpage' );
+    }
+}
+
+function conditional_urlads_process_request() {
+    if ( isset( $_POST[ 'nonce' ] ) && yourls_verify_nonce( 'conditional-urlads' ) ) {
+        ob_start();  // 开始输出缓冲
+        ob_end_clean();  // 清空输出缓冲
+        header('Content-Type: application/json');
+        #if ( isset( $_POST[ 'adfly_id' ], $_POST[ 'adfoc_id' ], $_POST[ 'ouoio_id' ], $_POST[ 'linkvertise_id' ], ) )
+        if ( isset( $_POST[ 'adfly_id' ] ) ) {
+            yourls_update_option( 'conditional_urlads_adfly_id', $_POST[ 'adfly_id' ] );
+        }
+        if ( isset( $_POST[ 'adfoc_id' ] ) ) {
+            yourls_update_option( 'conditional_urlads_adfoc_id', $_POST[ 'adfoc_id' ] );
+        }
+        if ( isset( $_POST[ 'ouoio_id' ] ) ) {
+            yourls_update_option( 'conditional_urlads_ouoio_id', $_POST[ 'ouoio_id' ] );
+        }
+        if ( isset( $_POST[ 'linkvertise_id' ] ) ) {
+            yourls_update_option( 'conditional_urlads_linkvertise_id', $_POST[ 'linkvertise_id' ] );
+        }
+        yourls_update_option( 'conditional_urlads_random_adurl_bool', isset( $_POST[ 'random_adurl_bool' ] ) );
+        $response = array('success' => true, 'message' => yourls__( 'Save Complete', 'conditional_urlads' ));
+        echo json_encode($response);  // 输出 JSON 数据
+        exit;  // 终止脚本执行，确保只返回 JSON 数据
+    }
+
+}
+
+function conditional_urlads_loadpage() {
+    #$nonce = yourls_create_nonce( 'conditional-urlads' );
+    include 'settings.php';
+}
+define( 'ADFLY_ID', yourls_get_option( 'conditional_urlads_adfly_id' ) );// Replace this with your Adfly ID
+define( 'ADFOCUS_ID', yourls_get_option( 'conditional_urlads_adfoc_id' ) );// Replace this with your Adfoc.us ID
+define( 'OUO_ID', yourls_get_option( 'conditional_urlads_ouoio_id' ) );// You get the drill
+define( 'LINKVERTISE_ID', yourls_get_option( 'conditional_urlads_linkvertise_id' ) );// You get the drill
+define( 'RANDOM_ADURL_BOOL', yourls_get_option( 'conditional_urlads_random_adurl_bool' ) );
+define( 'ADFLY_DOMAIN', 'https://adf.ly' );// If you have a custom Adfly domain, replace this with it
+define( 'ADFOCUS_DOMAIN', 'https://adfoc.us' );// Same for this
+define( 'OUO_DOMAIN', 'https://ouo.io' );
+
+yourls_add_action( 'loader_failed', 'check_for_redirect' );// On url fail, check here
 function check_for_redirect( $args ) {
-  $regex = '!^'. implode($TRIGGERS,"|") .'(.*)!'; // Match any trigger
-  if( preg_match( $regex, $args[0], $matches ) ){
-    define( redirectService, $matches[0][0] ); // first charachter of the redirect == service to use
-    $keyword = substr(yourls_sanitize_keyword( $matches[1] ), 1); // The new keyword, sub trigger
-    define(doAdvert, true); // let our advert function know to redirect
-    yourls_add_filter('redirect_location', 'redirect_to_advert'); // Add our ad-forwarding function
-    include( YOURLS_ABSPATH.'/yourls-go.php' ); // Retry forwarding
-    exit; // We already restarted the process, stop
-  }
+    $regex = '!^'. implode( $TRIGGERS, '|' ) .'(.*)!';
+    // Match any trigger
+    if ( preg_match( $regex, $args[ 0 ], $matches ) ) {
+        define( redirectService, $matches[ 0 ][ 0 ] );
+        // first charachter of the redirect == service to use
+        $keyword = substr( yourls_sanitize_keyword( $matches[ 1 ] ), 1 );
+        // The new keyword, sub trigger
+        define( doAdvert, true );
+        // let our advert function know to redirect
+        yourls_add_filter( 'redirect_location', 'redirect_to_advert' );
+        // Add our ad-forwarding function
+        include( YOURLS_ABSPATH.'/yourls-go.php' );
+        // Retry forwarding
+        exit;
+        // We already restarted the process, stop
+    }
 }
 
-define( 'TRIGGERS', array('a/', 'f/', 'o/', 'l/','r/') ); // Add any possible trigger to use here
+define( 'TRIGGERS', array( 'a/', 'f/', 'o/', 'l/', 'r/' ) );// Add any possible trigger to use here
 function redirect_to_advert( $url, $code ) {
-  if ( doAdvert ) {
-    $redirectUrl = getRedirect();
-    switch ( redirectService ) { 
-      case 'f': // Use adfocus
-        return ADFOCUS_DOMAIN . '/serve/sitelinks/?id=' . ADFOCUS_ID . '&url=' . $redirectUrl;
-      case 'a': // Adfly
-        return ADFLY_DOMAIN . '/' . ADFLY_ID . '/' . $redirectUrl;
-      case 'o': // OUO.io
-        return OUO_DOMAIN . '/qs/' . OUO_ID . '?s=' . $redirectUrl;
-      case 'l': // linkvertise.com
-        return getLinkvertise(LINKVERTISE_ID, $redirectUrl);
-      case 'r': //Random AdUrl
-        if( RANDOM_ADURL_BOOL ){
-          $keywords = ['a', 'f', 'o', 'l'];
-          return getRedirect($keywords[rand(0, 3)]);
+    if ( doAdvert ) {
+        $redirectUrl = getRedirect();
+        switch ( redirectService ) {
+            case 'f': // Use adfocus
+            return ADFOCUS_DOMAIN . '/serve/sitelinks/?id=' . ADFOCUS_ID . '&url=' . $redirectUrl;
+            case 'a': // Adfly
+            return ADFLY_DOMAIN . '/' . ADFLY_ID . '/' . $redirectUrl;
+            case 'o': // OUO.io
+            return OUO_DOMAIN . '/qs/' . OUO_ID . '?s=' . $redirectUrl;
+            case 'l': // linkvertise.com
+            return getLinkvertise( LINKVERTISE_ID, $redirectUrl );
+            case 'r': //Random AdUrl
+            if ( RANDOM_ADURL_BOOL ) {
+                $keywords = [ 'a', 'f', 'o', 'l' ];
+                return getRedirect( $keywords[ rand( 0, 3 ) ] );
+            }
         }
     }
-  }
-  return $url; // If none of those redirect services, forward to the normal URL
+    return $url;
+    // If none of those redirect services, forward to the normal URL
 }
 
-function getRedirect($keyword = null){
-  $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") ;
-  $actual_link = $protocol . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; // get the current url
-  $pieces = explode('/', $actual_link); // split the url into an arrray seperated by /
-  $last_word = array_pop($pieces); //  get the keyword - this may not work if you use a plugin to allow slashes in your shortened url
-  $redirect_url = $protocol . '://' . $_SERVER['SERVER_NAME'];
-
-  if ($keyword !== null) {
-      $redirect_url .= '/' . $keyword . '/' . $last_word;
-  } else {
-      $redirect_url .= '/' . $last_word;
-  }
-
-  return $redirect_url;
+function getRedirect( $keyword = null ) {
+    $protocol = ( isset( $_SERVER[ 'HTTPS' ] ) && $_SERVER[ 'HTTPS' ] === 'on' ? 'https' : 'http' ) ;
+    $actual_link = $protocol . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    // get the current url
+    $pieces = explode( '/', $actual_link );
+    // split the url into an arrray seperated by /
+    $last_word = array_pop( $pieces );
+    //  get the keyword - this may not work if you use a plugin to allow slashes in your shortened url
+    $redirect_url = $protocol . '://' . $_SERVER[ 'SERVER_NAME' ];
+    if ( $keyword !== null ) {
+        $redirect_url .= '/' . $keyword . '/' . $last_word;
+    } else {
+        $redirect_url .= '/' . $last_word;
+    }
+    return $redirect_url;
 }
 
 // About Linkvertise
-function getLinkvertise($userid, $link) {
-  $base_url = "https://link-to.net/" . $userid . "/" . strval(rand()*1000) . "/dynamic";
-  $href = $base_url . "?r=" . base64_encode(utf8_encode($link));
-  return $href;
+function getLinkvertise( $userid, $link ) {
+    $base_url = 'https://link-to.net/' . $userid . '/' . strval( rand()*1000 ) . '/dynamic';
+    $href = $base_url . '?r=' . base64_encode( utf8_encode( $link ) );
+    return $href;
 }
--- settings.php
+++ settings.php
@@ -1,52 +1,106 @@
 <?php
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+function add_title_label($websile, $id_name, $string, $url){
+    $id = yourls_get_option('conditional_urlads_' . $id_name);
+    echo <<<HTML
+    <tr valign="center">
+        <th scope="row">{$websile}</th>
+        <th><input type="text" name="{$id_name}" value='{$id}' /></th>
+        <th>a/</th>
+        <th><a href="{$url}">{$url}</a></th>
+    </tr>
+HTML;
+}
+
+function display_settings_table() {
+    $lang_syuid=yourls__( 'Set your user ID', 'conditional_urlads' );
+    $lang_yc=yourls__( 'You can', 'conditional_urlads' );
+    $lang_stfd=yourls__( 'See Tutorial for Details', 'conditional_urlads' );
+    $lang_web=yourls__( 'Website', 'conditional_urlads' );
+    $lang_uid=yourls__( 'User API ID', 'conditional_urlads' );
+    $lang_ts=yourls__( 'Trigger String', 'conditional_urlads' );
+    $lang_eurl=yourls__( 'Example URL', 'conditional_urlads' );
+    $nonce=yourls_create_nonce( 'conditional-urlads' );
+    echo <<<HTML
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('.inline-form').submit(function(e) {
+            e.preventDefault();
+            var formData = $(this).serialize();
+            $.ajax({
+                type: 'POST',
+                url: 'plugins.php?page=conditional-urlads',
+                data: formData,
+                dataType: 'json',
+                success: function(response) {
+                    if (response.success) {
+                        alert(response.message);
+                    } else {
+                        // 处理失败的操作
+                        alert('处理失败');
+                    }
+                },
+                error: function() {
+                    // 请求失败的操作
+                    alert('请求失败');
+                }
+            });
+        });
+    });
+    </script>
+    <style>
+    .inline-form {
+        display: inline;
+    }
+    </style>
+    <div class="wrap">
+        <h2>Conditional URLAds</h2>
+        <h4>
+            {$lang_syuid} （{$lang_yc}<a href="https://github.com/HeroCC/yourls-conditional_urlads">{$lang_stfd} </a>）<br>
+        </h4>
+        <form method="post" class="inline-form" >
+        <input type="hidden" name="nonce" value="{$nonce}" />
+            <table class="form-table">
+                <tr valign="center" algin="char">
+                    <th><h4>{$lang_web}</h4></th>
+                    <th><h4>{$lang_uid}</h4></th>
+                    <th><h4>{$lang_ts}</h4></th>
+                    <th><h4>{$lang_eurl}</h4></th>
+                </tr>
+HTML;
+}
+
+// Add more add_title_label calls as needed
+
+function display_settings_table_end() {
+    $lang_radurl=yourls__( 'Ramdom AdURL', 'conditional_urlads' );
+    $radurl_bool=yourls_get_option('conditional_urlads_random_adurl_bool') ? 'checked' : '';
+    $lang_enable=yourls__( 'Enable', 'conditional_urlads' );
+    $lang_save=yourls__( 'Save', 'conditional_urlads' );
+    echo <<<HTML
+                <tr valign="center">
+                    <th scope="row">{$lang_radurl}</th>
+                    <td>
+                        <input type="checkbox" id="random_adurl_bool" name="random_adurl_bool" value="true" {$radurl_bool} />
+                        <label for="random_adurl_bool">{$lang_enable}</label>
+                    </td>
+                    <th>r/</th>
+                    <th><a href="https://8mi.ink/r/2345">https://8mi.ink/r/2345</a></th>
+                </tr>
+            </table>
+            <br />
+            <input type="submit" value="{$lang_save}" class="button" />
+        </form>
+    </div>
+HTML;
+}
+
+display_settings_table();
+add_title_label('Adf.ly', 'adfly_id', 'a/', 'https://8mi.ink/a/ref-adfly');
+add_title_label('Adfoc.us', 'adfoc_id', 'f/', 'https://8mi.ink/f/ref-adfoc');
+add_title_label('OuO.io', 'ouoio_id', 'o/', 'https://8mi.ink/o/ref-ouoio');
+add_title_label('Linkvertise', 'linkvertise_id', 'l/', 'https://8mi.ink/l/ref-linkvertise');
+display_settings_table_end();
 ?>
-<div class="wrap">
-    <h2>Conditional URLAds</h2>
-    <h4><?php yourls_e( 'Set your user ID', 'conditional_urlads' ); ?> （<?php yourls_e( 'You can', 'conditional_urlads' ); ?> <a href="https://github.com/HeroCC/yourls-conditional_urlads"><?php yourls_e( 'See Tutorial for Details', 'conditional_urlads' ); ?> </a>）<br><?php yourls_e( 'Current Language', 'conditional_urlads' ); ?> <?php echo yourls_apply_filter( 'load_custom_textdomain', yourls_get_locale(), 'conditional_urlads' );; ?></h4>
-    <form method="post">
-    <input type="hidden" name="nonce" value="<?php echo yourls_create_nonce( 'conditional_urlads_settings' ); ?>" />
-        <table class="form-table">
-            <tr valign="center" algin="char">
-                <th><h4><?php yourls_e( 'Website', 'conditional_urlads' ); ?></h4></th>
-                <th><h4><?php yourls_e( 'User API ID', 'conditional_urlads' ); ?></h4></th>
-                <th><h4><?php yourls_e( 'Trigger String', 'conditional_urlads' ); ?></h4></th>
-                <th><h4><?php yourls_e( 'Example URL', 'conditional_urlads' ); ?></h4></th>
-            </tr>
-            <tr valign="center">
-                <th scope="row">Adfly</th>
-                <td><input type="text" name="adfly_id" value="<?php echo yourls_get_option('conditional_urlads_adfly_id'); ?>" /></td>
-                <th>a/</th>
-                <th><a href="https://8mi.ink/a/ref-adfly">https://8mi.ink/a/ref-adfly</a></th>
-            </tr>
-            <tr valign="center">
-                <th scope="row">Adfoc.us</th>
-                <td><input type="text" name="adfoc_id" value="<?php echo yourls_get_option('conditional_urlads_adfoc_id'); ?>" /></td>
-                <th>f/</th>
-                <th><a href="https://8mi.ink/f/ref-adfoc">https://8mi.ink/f/ref-adfoc</a></th>
-            </tr>
-            <tr valign="center">
-                <th scope="row">OUO.io</th>
-                <td><input type="text" name="ouoio_id" value="<?php echo yourls_get_option('conditional_urlads_ouoio_id'); ?>" /></td>
-                <th>o/</th>
-                <th><a href="https://8mi.ink/o/ref-ouoio">https://8mi.ink/o/ref-ouoio</a></th>
-            </tr>
-            <tr valign="center">
-                <th scope="row"><a href="https://publisher.linkvertise.com/dashboard#dynamic">Linkvertise</a></th>
-                <td><input type="text" name="linkvertise_id" value="<?php echo yourls_get_option('conditional_urlads_linkvertise_id'); ?>" /></td>
-                <th>l/</th>
-                <th><a href="https://8mi.ink/l/ref-linkvertise">https://8mi.ink/l/ref-linkvertise</a></th>
-            </tr>
-            <tr valign="center">
-                <th scope="row"><?php yourls_e( 'Ramdom AdURL', 'conditional_urlads' ); ?></th>
-                <td>
-                    <input type="checkbox" id="random_adurl_bool" name="random_adurl_bool" value="true" <?php echo yourls_get_option('conditional_urlads_random_adurl_bool') ? 'checked' : ''; ?> />
-                    <label for="random_adurl_bool"><?php yourls_e( 'Enable', 'conditional_urlads' ); ?></label>
-                </td>
-                <th>r/</th>
-                <th><a href="https://8mi.ink/r/2345">https://8mi.ink/r/2345</a></th>
-            </tr>
-        </table>
-        <br />
-        <input type="submit" value="<?php yourls_e( 'Save', 'conditional_urlads' ); ?>" class="button" />
-    </form>
